5965a4e4fbac5df4f1008bbb7aa664c3c04f07bb
Gioelinz <gioelerunza.gr@gmail.com>
2021-12-20 15:21:53 +0100

    add analyses

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log("JS OK")
+
+/* Traccia
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+1- Creare variabili per Nome, Cognome, Colori
+2- Creare prompt e popups
+3- stampare variabili
+4- inserire testo su HTML
+*/
+
