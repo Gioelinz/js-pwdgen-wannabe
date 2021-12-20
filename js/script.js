07967293e9e7ce8ab101d8a4950cf9ddff272a3a
Gioelinz <gioelerunza.gr@gmail.com>
2021-12-20 15:48:48 +0100

    add variables & stamp in html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-console.log("JS OK")
 
 /* Traccia
 Chiedi all’utente il suo nome,
@@ -11,4 +10,32 @@ Infine scrivi sulla pagina nomecognomecolorepreferito21
 3- stampare variabili
 4- inserire testo su HTML
 */
+console.log("JS OK");
 
+//Benvenuto
+alert("Ciao! Se sei qui è perchè vuoi creare una nuova password più sicura?! Allora clicca su Ok");
+
+//! Creazione Variabili & stamparle
+
+//Nome
+const firstName = prompt("Qual'è il tuo Nome?");
+
+console.log(firstName);
+
+//Cognome
+
+const lastName = prompt("Qual'è il tuo Cognome?");
+
+console.log(lastName);
+
+//Colore
+
+const favColour = prompt("Qual'è il tuo colore preferito?");
+
+console.log(favColour);
+
+//! Inserire testo in HTML
+
+const newPSW = document.getElementById('yourpsw');
+
+newPSW.innerHTML = `La tua nuova password è: ${firstName}${lastName}${favColour}21`
